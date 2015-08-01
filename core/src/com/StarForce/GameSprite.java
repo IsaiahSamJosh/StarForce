a9587426ea9cc2d9c8e8d5916919ff2102555e87
flysaiah <mayeis01@yahoo.com>
2015-07-31 20:42:07 -0500

    Added new GameSprite class

---
+++ core/src/com/StarForce/GameSprite.java
@@ -0,0 +1,13 @@
+package com.StarForce;
+
+import com.badlogic.gdx.physics.box2d.Body;
+
+public class GameSprite {
+	
+	Body body;
+	
+	public GameSprite(Body phBody) {
+		this.body = phBody;
+	}
+	
+}
