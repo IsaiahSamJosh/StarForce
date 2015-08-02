82d8b7e26a4ce3b451c4ccb3381729db05e905e5
flysaiah <mayeis01@yahoo.com>
2015-08-02 17:08:32 -0500

    Added physics to ground tile layer, with friction

--- core/src/com/StarForce/WorldRenderer.java
+++ core/src/com/StarForce/WorldRenderer.java
@@ -9,6 +9,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
@@ -16,6 +18,7 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.ChainShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
@@ -53,7 +56,7 @@ public class WorldRenderer {
 		//resizes the screen based on the width and the height of the level
 	}
 	public WorldRenderer(StarForce game) {//first param was World world not Level level!
-		this.world = new World(new Vector2(0f, 0f), true);
+		this.world = new World(new Vector2(0f, -10000f), true);
 		this.b2dr = new Box2DDebugRenderer();
 		this.tx=new Textures();
 		TmxMapLoader loader = new TmxMapLoader();
@@ -68,17 +71,80 @@ public class WorldRenderer {
 		PolygonShape shape = new PolygonShape();
 		
 		// create player
-		bdef.position.set(30,20);
+		bdef.position.set(600,900);
 		bdef.type = BodyType.DynamicBody;
 		Body body = world.createBody(bdef);
 		shape.setAsBox(Hero.SIZE/2f, Hero.SIZE/2f);
 		fdef.shape = shape;
+		fdef.filter.categoryBits = 0x10;
+		fdef.filter.maskBits = 0x01;
 		body.createFixture(fdef);
 		
 		this.hero = new Hero(body);
 		spriteBatch = new SpriteBatch();
 		
 		loadTextures();
+		createTiles();
+	}
+	private void createTiles() {
+		TmxMapLoader loader = new TmxMapLoader();
+        map = loader.load("Level1.tmx");
+        renderer2 = new OrthogonalTiledMapRenderer(map);
+		
+		// load tile map
+		Integer tileSize = (Integer) (map.getProperties().get("tilewidth"));
+		
+		TiledMapTileLayer layer;
+		
+		layer = (TiledMapTileLayer) map.getLayers().get("Ground");
+		System.out.println(map.getLayers().get("Ground"));
+		createLayer(layer, tileSize);
+		
+		
+	}
+	
+	private void createLayer(TiledMapTileLayer layer, Integer tileSize) {
+		
+		BodyDef bdef = new BodyDef();
+		FixtureDef fdef = new FixtureDef();
+		
+		// go through all the cells in the layer
+		for(int row = 0; row < layer.getHeight(); row++) {
+			for(int col = 0; col < layer.getWidth(); col++) {
+				
+				// get cell
+				Cell cell = layer.getCell(col, row);
+				
+				// check if cell exists
+				if(cell == null) continue;
+				if(cell.getTile() == null) continue;
+				
+				// create a body + fixture from cell
+				bdef.type = BodyType.StaticBody;
+				bdef.position.set(
+					(col + 0.5f) * tileSize,
+					(row + 0.5f) * tileSize
+				);
+				
+				ChainShape cs = new ChainShape();
+				Vector2[] v = new Vector2[3];
+				v[0] = new Vector2(
+					-tileSize / 2, -tileSize / 2);
+				v[1] = new Vector2(
+					-tileSize / 2, tileSize / 2);
+				v[2] = new Vector2(
+					tileSize / 2, tileSize / 2);
+				cs.createChain(v);
+				fdef.friction = 1f;
+				fdef.shape = cs;
+				fdef.filter.categoryBits = 0x01;
+				fdef.filter.maskBits = 0x10;
+				fdef.isSensor = false;
+				world.createBody(bdef).createFixture(fdef);
+				
+				
+			}
+		}
 	}
 	
 	public Hero getHero() {
