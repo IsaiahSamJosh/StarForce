94d7ab4b28f3baa3bcdd8b72ba52736706934132
flysaiah <mayeis01@yahoo.com>
2015-08-07 19:37:20 -0500

    Changed deprecated debug method in MMScreen

--- core/src/com/StarForce/MainMenuScreen.java
+++ core/src/com/StarForce/MainMenuScreen.java
@@ -35,7 +35,7 @@ public class MainMenuScreen implements Screen {
 			stage.act(Gdx.graphics.getDeltaTime());
 	        stage.draw();
 
-			Table.drawDebug(stage); // This is optional, but enables debug lines for tables.
+			stage.setDebugAll(true); // This is optional, but enables debug lines for tables.
 			
 		}
 
